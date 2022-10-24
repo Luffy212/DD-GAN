f09fc4ce1333c20477e1c470f31579ea863952df
Luffy212 <39082275+Luffy212@users.noreply.github.com>
2022-10-24 17:02:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import random
 import numpy as np
 import torch
 # ----------------------------------------------
-from models.DD_GAN_V1 import DD_GAN_V1
+from models.DD_GAN import DD_GAN
+
 # ----------------------------------------------
 
 def parse_args():
@@ -15,7 +16,7 @@ def parse_args():
     parser.add_argument('--workers', type=int, default=4, help='number of data loading workers')
     parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
     parser.add_argument('--batch_size', type=int, default=50, help='input batch size [default: 30]')
-    parser.add_argument('--learning_rate', type=float, default=0.0001, help='Initial learning rate [default: 0.0001]')
+    parser.add_argument('--lr', type=float, default=0.0001, help='Initial learning rate [default: 0.0001]')
     parser.add_argument('--max_epoch', type=int, default=300, help='number of epochs to train for')
     parser.add_argument('--optimizer', default='adam', help='adam or momentum [default: adam]')
     parser.add_argument('--log_info', default='log_info.txt', help='log_info txt')
@@ -23,7 +24,6 @@ def parse_args():
     parser.add_argument('--checkpoint_dir', default='checkpoint', help='Checkpoint dir [default: checkpoint]')
     parser.add_argument('--snapshot', type=int, default=20, help='how many epochs to save model')
     parser.add_argument('--network', default=None, help='which network model to be used')
-    parser.add_argument('--sketch_lr', type=lambda x: utils.restricted_float(x, [1e-5, 0.5]), default=0.0001, metavar='LR', help='Initial learning rate [1e-5, 5e-4] (default: 1e-4)')
     parser.add_argument('--pretrain_model', default=None, help='use the pretrain model')
     return check_args(parser.parse_args())
 
@@ -63,8 +63,9 @@ def main():
 
     # create model
     print('****************network: {}****************'.format(args.network))
-    if args.network == 'DD_GAN_V1':
-        gan = DD_GAN_V1(args)
+    if args.network == 'DD_GAN':
+        gan = DD_GAN(args)
+
  
     else:
         print('select model error!!!')
@@ -82,18 +83,9 @@ def main():
 
         gan.train()
         print(" [*] Training finished!")
-    # exit()
     if args.phase == 'test' :
         gan.test()
         print(" [*] Test finished!")
-
-    if args.phase == 'extract' :
-        gan.evalueonline()
-        print(" [*] Test finished!")
-
-    if args.phase == 'extract_cat':
-        gan.extract_fea_concat()
-        print(" [*] Extract feature finished")
 
 if __name__ == '__main__':
     main()
